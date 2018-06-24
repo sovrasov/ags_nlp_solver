5e72b5fd198097087c809ae70a7ee16fcb6eabc2
sovrasov <vlads247@gmail.com>
2018-06-24 16:12:33 +0300

    Delete obsolete code

--- sample/TestGrishaginClass.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_GRISHAGIN_CLASS_HPP
-#define TEST_GRISHAGIN_CLASS_HPP
-
-#include "OptimizerDataStructures.hpp"
-
-int TestVAGrisClass(optimizercore::OptimizerParameters algParams);
-
-#endif
