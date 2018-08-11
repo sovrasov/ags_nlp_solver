a8bba440632b9264d639695540764838222b5def
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-08-11 17:39:14 +0300

    Add missing license header

--- solver/bindings_src/py_bindings.cpp
+++ solver/bindings_src/py_bindings.cpp
@@ -1,3 +1,10 @@
+/*
+Copyright (C) 2018 Sovrasov V. - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ * You should have received a copy of the MIT license with
+ * this file. If not visit https://opensource.org/licenses/MIT
+*/
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
