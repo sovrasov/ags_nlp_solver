23debd838e8c0fcdcff29ded2a88e1aa76cacd89
sovrasov <sovrasov.vlad@gmail.com>
2018-08-13 00:25:14 +0300

    Update sample

--- samples/python/solve_set.py
+++ samples/python/solve_set.py
@@ -1,11 +1,20 @@
 import ags_solver
 import go_problems
-from benchmark_tools.core import Solver, solve_class, grish_class, gkls_class
+from benchmark_tools.core import Solver, solve_class, GrishClass, GKLSClass
 from benchmark_tools.plot import plot_cmcs
 from benchmark_tools.stats import save_stats, compute_stats
 
-class AGSWrapper(Solver):
+import numpy as np
 
+class AGSWrapper(Solver):
+    def __init__(self, solver):
+        self.solver = solver
+
+    def Solve(self, problem):
+        self.solver.SetProblem(problem)
+        point, val, idx = self.solver.Solve()
+        calcCounters = self.solver.GetCalculationsStatistics()
+        return point, val, calcCounters
 
 def main():
 
@@ -13,8 +22,9 @@ def main():
     solver = ags_solver.Solver()
     solver.SetParameters(params)
 
-    results = solve_class(grish_class(), AGSWrapper(solver))
-    cmc_curve, avg_calculations = compute_stats(results)
+    problems = GrishClass()
+    calc_stats, num_solved = solve_class(problems, AGSWrapper(solver))
+    cmc_curve, avg_calculations = compute_stats(calc_stats)
     plot_cmc(cmc_curve)
     save_stats(cmc_curve, avg_calculations)
 
