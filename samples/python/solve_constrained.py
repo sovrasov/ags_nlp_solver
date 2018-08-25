fd6ac27845a36908f6a77ba3234cddb322c254d3
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-08-25 13:37:28 +0300

    Add visualization to samples

--- samples/python/solve_constrained.py
+++ samples/python/solve_constrained.py
@@ -1,8 +1,18 @@
 import ags_solver
 import math
 import argparse
+try:
+    import sys
+    sys.path.append('../3rd-party/global-optimization-test-problems/')
+    from benchmark_tools.plot import plot_countours
+    plot_found = True
+except ImportError:
+    plot_found = False
 
+trials = []
 def c1(x):
+    global trials
+    trials.append(x)
     return 0.01*(math.pow(x[0] - 2.2, 2) + math.pow(x[1] - 1.2, 2) - 2.25)
 def c2(x):
     return 100 * (1 - math.pow(x[0] - 2, 2) / 1.44 - math.pow(0.5*x[1], 2))
@@ -46,12 +56,15 @@ def main(args):
         print('Optimal value: {}'.format(val))
         print('x = ' + str(point[0]) + ' y = ' + str(point[1]))
 
+    if plot_found:
+        plot_countours([c1, c2, c3, obj_f], bounds, '', points=trials, show=True)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sample for AGS solver')
     parser.add_argument('--m', type=int, default=12, help='Evolvent density')
     parser.add_argument('--r', type=float, default=3, help='Reliability parameter for the solver')
-    parser.add_argument('--eps', type=float, default=0.001, help='Accuracy of the method')
-    parser.add_argument('--epsR', type=float, default=0.01, help='eps-reserves for all constraints')
+    parser.add_argument('--eps', type=float, default=0.01, help='Accuracy of the method')
+    parser.add_argument('--epsR', type=float, default=0.1, help='eps-reserves for all constraints')
     parser.add_argument('--max_iters', type=int, default=10000, help='limit of iterations for the method')
     parser.add_argument('--refine_loc', action='store_true', help='Refine the global solution using a local optimizer')
 
