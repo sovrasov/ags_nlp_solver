64299b3944d3e1aaa07ed24eb7b3868ee9a46c41
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-08-27 16:03:01 +0300

    Update scripts with experiments

--- experiments/run_gklsh5d.sh
+++ experiments/run_gklsh5d.sh
@@ -3,5 +3,5 @@ declare -a algos=("ags" "direct" "directl" "mlsl" "crs" "scd" "stogo")
 for algo in "${algos[@]}"
 do
    echo "$algo"
-   python2 ../samples/python/solve_different_methods.py --verbose --dist_stop --max_iters 5000 --problems_dim 2 --problems_class grish --algo $algo #--stats_fname ../experiments/grish/$algo.json
+   python2 ../samples/python/solve_different_methods.py --verbose --dist_stop --max_iters 600000 --problems_dim 5 --problems_class gklsh --algo $algo --stats_fname ../experiments/gklsh5d/$algo.json
 done
--- experiments/run_gklss5d.sh
+++ experiments/run_gklss5d.sh
@@ -3,5 +3,5 @@ declare -a algos=("ags" "direct" "directl" "mlsl" "crs" "scd" "stogo")
 for algo in "${algos[@]}"
 do
    echo "$algo"
-   python2 ../samples/python/solve_different_methods.py --verbose --dist_stop --max_iters 5000 --problems_dim 5 --problems_class gklss --algo $algo --stats_fname ../experiments/gklss5d/$algo.json
+   python2 ../samples/python/solve_different_methods.py --verbose --dist_stop --max_iters 350000 --problems_dim 5 --problems_class gklss --algo $algo --stats_fname ../experiments/gklss5d/$algo.json
 done
