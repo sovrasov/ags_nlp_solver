7ad4f3e45a17561e812b624869cf7cca7c1b4d38
sovrasov <sovrasov.vlad@gmail.com>
2018-09-07 23:34:13 +0300

    Use tabulate to generate latex tables

--- experiments/generate_report.py
+++ experiments/generate_report.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import csv
 import os
+from tabulate import tabulate
 
 def main(args):
     all_stats = []
@@ -19,14 +20,21 @@ def main(args):
         if len(directory_stats):
             all_stats.append((directory_stats, os.path.basename(subdir)))
 
+    columns = ['Метод', 'Среднее число испытаний', 'Решено задач']
     with open(os.path.join(args.root_folder, 'report.csv'), 'w') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['class', 'method', 'solved', 'avg trials'])
         for class_results, class_name in all_stats:
+            print('Results on ' + class_name)
+            table_rows = []
             for method_results in class_results:
+                table_rows.append((method_results['capture'], method_results['calc_counters'][0], method_results['num_solved']))
                 writer.writerow([class_name, method_results['capture'],
                     method_results['num_solved'], round(method_results['calc_counters'][0])])
             writer.writerow([])
+            table = tabulate(sorted(table_rows), headers=columns, tablefmt="latex", floatfmt=".2f")
+            print(table)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
