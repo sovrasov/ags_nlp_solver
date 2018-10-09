72d0530d94d4f4bf64962231051c5398b9c3529b
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-10-09 14:32:17 +0300

    Add results of AGSl

--- experiments/run_gklsh4d_serg.sh
+++ experiments/run_gklsh4d_serg.sh
@@ -5,4 +5,5 @@ do
    echo "$algo"
    python2 ../experiments/solve_different_methods.py --serg_eps --verbose --dist_stop --max_iters 250000 --problems_dim 4 --problems_class gklsh --algo $algo --stats_fname ../experiments/gklsh4d_serg/$algo.json
 done
+python2 ../experiments/solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture AGSl --verbose --max_iters 250000 --problems_dim 4 --problems_class gklsh --r 4.9 --lm 5 --serg_eps --stats_fname ../experiments/gklsh4d_serg/agsl_examin.json
 python2 ../experiments/plot_cmcs.py ../experiments/gklsh4d_serg/ --show
--- experiments/run_gklss2d.sh
+++ experiments/run_gklss2d.sh
@@ -5,4 +5,5 @@ do
    echo "$algo"
    python2 ../experiments/solve_different_methods.py --verbose --dist_stop --max_iters 8000 --problems_dim 2 --problems_class gklss --algo $algo --stats_fname ../experiments/gklss2d/$algo.json
 done
+python2 ../experiments/solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture AGSl --verbose --max_iters 8000 --problems_dim 2 --problems_class gklss --r 4.6 --lm 5 --eps 0.01 --stats_fname ../experiments/gklss2d/agsl_examin.json
 python2 ../experiments/plot_cmcs.py ../experiments/gklss2d/ --show
