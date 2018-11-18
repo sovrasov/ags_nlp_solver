2bce9c7c5b9435ea960f3e7faf503d8fa55dcca8
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-11-18 22:46:16 +0300

    Update scripts for cluster

--- experiments/run_clust_gklss4d.sh
+++ experiments/run_clust_gklss4d.sh
@@ -6,13 +6,14 @@ declare -a thr_nums=("1" "16")
 iters=150000
 class="gklss"
 folder="mpi/gklss4d"
-patrition="-p cpu"
+partition="-p cpu"
 complexity="0"
 
 for n_num in "${n_nodes[@]}"
 do
+preffix="srun -N $n_num $partition"
 for thr_num in "${thr_nums[@]}"
   do
-    python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --eps 0.01 --problems_class $class --problems_dim 4 --lm 0 --r 4.7 --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix 'srun -N $n_num $patrition' --complexity $complexity
+    python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --eps 0.01 --problems_class $class --problems_dim 4 --lm 0 --r 4.7 --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
   done
 done
