110fa8b75097ff6fcfce9ed5e6820e01df3fd2f2
sovrasov <sovrasov.vlad@gmail.com>
2018-11-19 21:25:34 +0300

    Update mpi run scripts

--- experiments/run_clust_gklsh4d.sh
+++ experiments/run_clust_gklsh4d.sh
@@ -7,11 +7,11 @@ iters=250000
 class="gklsh"
 folder="mpi/gklsh4d"
 partition="-p gpu"
-complexity="11000"
+complexity="20000"
 
 for n_num in "${n_nodes[@]}"
 do
-preffix="srun -N $n_num $partition"
+preffix="srun -N $n_num $partition -t 600"
 for thr_num in "${thr_nums[@]}"
   do
     python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --eps 0.01 --problems_class $class --problems_dim 4 --lm 0 --r 4.9 --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
--- experiments/run_clust_gklss4d_serg.sh
+++ experiments/run_clust_gklss4d_serg.sh
@@ -7,11 +7,11 @@ iters=150000
 class="gklss"
 folder="mpi/gklss4d_serg"
 partition="-p gpu"
-complexity="11000"
+complexity="20000"
 
 for n_num in "${n_nodes[@]}"
 do
-preffix="srun -N $n_num $partition"
+preffix="srun -N $n_num $partition -t 600"
 for thr_num in "${thr_nums[@]}"
   do
     python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --serg_eps --problems_class $class --problems_dim 4 --lm 0 --r 4.7 --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
