3401e4a088c1387b9bc4107707a9cf0c51f4e6b5
sovrasov <sovrasov.vlad@gmail.com>
2018-11-22 19:04:13 +0300

    Update run scripts

--- experiments/run_clust_gklsh4d.sh
+++ experiments/run_clust_gklsh4d.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -a n_nodes=("1" "2" "4" "8" "12")
+r_values=( ["1"]="4.9" ["2"]="4.6" ["4"]="4.4" ["8"]="4.0" ["12"]="3.7")
 declare -a thr_nums=("1" "16")
 
 iters=250000
@@ -14,6 +15,6 @@ do
 preffix="srun -N $n_num $partition -t 600"
 for thr_num in "${thr_nums[@]}"
   do
-    python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --eps 0.01 --problems_class $class --problems_dim 4 --lm 0 --r 4.9 --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
+    python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --eps 0.01 --problems_class $class --problems_dim 4 --lm 0 --r "${r_values[$n_num]}" --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
   done
 done
--- experiments/run_clust_gklss4d_serg.sh
+++ experiments/run_clust_gklss4d_serg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -a n_nodes=("1" "2" "4" "8" "12")
+r_values=( ["1"]="4.7" ["2"]="4.4" ["4"]="4.1" ["8"]="3.8" ["12"]="3.5")
 declare -a thr_nums=("1" "16")
 
 iters=150000
@@ -14,6 +15,6 @@ do
 preffix="srun -N $n_num $partition -t 600"
 for thr_num in "${thr_nums[@]}"
   do
-    python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --serg_eps --problems_class $class --problems_dim 4 --lm 0 --r 4.7 --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
+    python2 solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture 'Parallel AGS' --verbose --max_iters $iters --serg_eps --problems_class $class --problems_dim 4 --lm 0 --r "${r_values[$n_num]}" --stats_fname $folder/examin_"$n_num"_"$thr_num".json --parallel_evolvent --ne $n_num --p $thr_num --preffix ''"$preffix"'' --complexity $complexity
   done
 done
