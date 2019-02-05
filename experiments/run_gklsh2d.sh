13cd814c191d965394d95e3d04dde7e16fec3e91
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-02-05 18:16:13 +0300

    Update experiments

--- experiments/run_gklsh2d.sh
+++ experiments/run_gklsh2d.sh
@@ -1,9 +1,9 @@
-declare -a algos=("ags" "direct" "directl" "mlsl" "crs" "scd" "stogo", "simple", "sda")
+declare -a algos=("ags") # "direct" "directl" "mlsl" "crs" "scd" "stogo", "simple", "sda")
 
 for algo in "${algos[@]}"
 do
    echo "$algo"
    python2 ../experiments/solve_different_methods.py --verbose --dist_stop --max_iters 9000 --problems_dim 2 --problems_class gklsh --algo $algo --stats_fname ../experiments/gklsh2d/$algo.json
 done
-python2 ../experiments/solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture AGSl --verbose --max_iters 9000 --problems_dim 2 --problems_class gklsh --r 6.5 --lm 5 --eps 0.01 --stats_fname ../experiments/gklsh2d/agsl_examin.json
+#python2 ../experiments/solve_examin.py --bin_path ../../globalizer/_bin/ --algo_capture AGSl --verbose --max_iters 9000 --problems_dim 2 --problems_class gklsh --r 6.5 --lm 5 --eps 0.01 --stats_fname ../experiments/gklsh2d/agsl_examin.json
 python2 ../experiments/plot_cmcs.py ../experiments/gklsh2d/ --show
